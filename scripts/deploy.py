21913ac0b3b21feaebabc069e23fab5ee77544fe
amitnil-developer <amit.nilajkar@gmail.com>
2021-10-19 02:06:42 +0530

    brownie git commit

---
+++ scripts/deploy.py
@@ -0,0 +1,18 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (get_account, deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENTS)
+from web3 import Web3
+
+def deploy_fund_me():
+    account = get_account()
+    #fund_me = FundMe.deploy({"from":account}, publish_source=True)\
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+    fund_me = FundMe.deploy("0x8A753747A1Fa494EC906cE90E9f37563A8AF630e", {"from":account}, publish_source= config["networks"][network.show_active()].get("verify"))
+    print(f"Contract deployed to {fund_me.address}")
+
+
+def main():
+    deploy_fund_me()
